9242c3d30c4a410047c4a6aa9cc209e1a4566d5d
dennosp254 <dennobasil2@gmail.com>
2024-02-12 01:35:36 +0300

    Models

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This file defines state Model class
+"""
+
+from model.base_model import BaseModel
+
+class State(BaseModel):
+	# State model
+	
+	# Attributes
+	name: str = ''
---
+++ models/user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This file inherits from BaseModel
+and defines UserModel class
+"""
+
+from models.base_model import BaseModel
+
+class User(BaselModel):
+	# User Model
+
+	#class attributes
+	email: str = ''
+	password: str = ''
+	first_name: str = ''
+	last_name: str = ''
